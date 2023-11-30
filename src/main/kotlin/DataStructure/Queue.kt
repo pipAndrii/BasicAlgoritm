c8b4e1e9d63fec7a0a66c1dba238e0c220ab42ad
andrii <andriipurskyi@gmail.com>
2023-11-30 19:38:56 +0200

    must change Queue and LinkedList

--- src/main/kotlin/DataStructure/Queue.kt
+++ src/main/kotlin/DataStructure/Queue.kt
@@ -1,54 +1,58 @@
 package DataStructure
 
-class Queue <T>(var element:T? = null,var link:Queue<T>? = null){
+class Queue <T>{
 
-    companion object {
-        var amount: UInt = 0u
-    }
-    init {
-        amount++
-    }
-    fun enQueue(newElement: T) {
-        if (element == null)
-        {
-            element = newElement
+    private var present: Node<T>? = null
+    fun size(): Int {
+        var count = 0
+        var current = present
+        while (current != null) {
+            count++
+            current = current.link!!.present
         }
-        else
-        {
-            var current = this
-            while (current.link != null)
-            {
-                current = current.link!!
-            }
-            current.link = Queue(newElement)
-
-        }
+        return count
     }
+//    fun enQueue(newElement: T) {
+//        if (element == null)
+//        {
+//            element = newElement
+//        }
+//        else
+//        {
+//            var current = this
+//            while (current.link != null)
+//            {
+//                current = current.link!!
+//            }
+//            current.link = Queue(newElement)
+//
+//        }
+//    }
+//
+//    fun deQueue():T?
+//    {
+//
+//        if (link == null)
+//        {
+//            element = null
+//            return element
+//        }
+//        else
+//        {
+//            var buff = this.element
+//            this.element = this.link!!.element
+//            this.link = this.link!!.link
+//            return buff
+//        }
+//        amount--
+//
+//    }
+//
+//    fun Print() {
+//        println(element)
+//        this.link?.Print()
+//    }
 
-    fun deQueue():T?
-    {
-
-        if (link == null)
-        {
-            element = null
-            return element
-        }
-        else
-        {
-            var buff = this.element
-            this.element = this.link!!.element
-            this.link = this.link!!.link
-            return buff
-        }
-        amount--
-        
-    }
-
-    fun Print() {
-        println(element)
-        this.link?.Print()
-    }
-
-
+    private inner class Node<T>(var element:T,var link:Queue<T>? = null){}
 
 }
